24f9d63c6e92c018f09a6407dd1fe552d9491774
Brandon Fuller <bjcfuller@uri.edu>
2018-11-07 16:39:09 -0500

    Show the featured image and title for supported links in the popup; no need for links to have an id anymore

--- inc/link-previews.php
+++ inc/link-previews.php
@@ -8,14 +8,22 @@
 function uri_modern_get_page_content_links( $content ) {
 
 	$dom = new DOMDocument;
-	$dom->loadHTML( $GLOBALS['post']->post_content );
+	@$dom->loadHTML( $GLOBALS['post']->post_content );
 	$links = $dom->getElementsByTagName( 'a' );
 
 	$a = array();
 
 	foreach ( $links as $link ) {
 		$id = uniqid();
-		$a[ $id ] = $link->getAttribute( 'href' );
+		$url = $link->getAttribute( 'href' );
+		$postID = url_to_postid( $url );
+		$a[ $id ] = array(
+			'url' => $url,
+			'postID' => $postID,
+			'title' => get_the_title( $postID ),
+			'thumb' => get_the_post_thumbnail( $postID ),
+			// 'excerpt' => get_the_excerpt( $postID ),
+		);
 	}
 
 	wp_localize_script( 'uri-modern-scripts', 'URIMODERN_LINKS', $a );
--- src/js/linkpreviews.js
+++ src/js/linkpreviews.js
@@ -8,24 +8,62 @@
 
 	'use strict';
 
+	var popup;
+
 	window.addEventListener( 'load', initLinkPreviews(), false );
 
 	function initLinkPreviews() {
-		
+
 		var links, content, i, key;
 
 		console.log( URIMODERN_LINKS );
-		
+
+		// Add the popup container div
+		popup = document.createElement( 'div' );
+		popup.id = 'link-preview-container';
+		document.getElementById( 'content' ).appendChild( popup );
+
+		// Get the links in the first .entry-content
 		content = document.querySelector( '.entry-content' );
 		links = content.querySelectorAll( 'a' );
-		
+
+		// Set ids and add event listeners
 		i = 0;
 		for ( key in URIMODERN_LINKS ) {
-			links[i].setAttribute( 'data-preview-id', key );
+			//links[i].setAttribute( 'data-preview-id', key );
+			links[i].addEventListener( 'mouseover', handleHover.bind( null, links[i], key ), false );
+			links[i].addEventListener( 'mouseout', handleOut, false );
 			i++;
 		}
-		
+
 		console.log( links );
+
+	}
+
+	function handleHover( el, id ) {
+
+		var img, title, x, y;
+		
+		if ( 0 == URIMODERN_LINKS[id]['postID'] ) {
+			return;
+		}
+
+		x = el.offsetTop;
+		y = el.offsetLeft;
+
+		img = URIMODERN_LINKS[id]['thumb'];
+		title = URIMODERN_LINKS[id]['title'];
+
+		popup.innerHTML = img + '<br >' + title;
+		popup.style.left = y + 'px';
+		popup.style.top = 'calc( 4rem + ' + x + 'px)';
+		popup.className = 'visible';
+
+	}
+
+	function handleOut() {
+
+		popup.className = 'hidden';
 
 	}
 
